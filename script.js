c3205156c716d0a27521d22c55892634d8af4460
anik-das <anikdas1085@gmail.com>
2024-10-04 14:23:55 +0600

    only structure done

---
+++ script.js
@@ -0,0 +1,7 @@
+const loadAllPost = async () =>{
+    const response= await fetch("https://openapi.programming-hero.com/api/retro-forum/posts")
+    const data = await response.json();
+    console.log(data.posts);
+
+}
+loadAllPost();
